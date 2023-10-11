b8f4772d6d2ad38c95c8a97fed7b2078dc26ff17
Vlad Borodaev <vladimir.borodaev@itp-it.com>
2023-10-11 18:57:57 +0300

    feat(mz): Add examples for mz

---
+++ src/mz-example.js
@@ -0,0 +1,16 @@
+// For older Node.js versions which don't support currenr EcmaScript specifications
+
+const fs = require("mz/fs");
+
+fs.exists(__filename)
+    .then((exists) => {
+        if (exists) {
+            console.debug("Exists!")
+        }
+    });
+
+
+const exec = require("mz/child_process").exec;
+
+exec("node -v")
+    .then((stdout) => console.info(stdout));
